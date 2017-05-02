e96ebbe6238309ec992a965d03c12129a8532fe0
charyorde <dreyemi@gmail.com>
2017-05-02 09:15:49 +0100

    build commit

--- ci/build-rst.sh
+++ ci/build-rst.sh
@@ -10,5 +10,20 @@ pip install recommonmark
 #sphinx-build -b html resource-gcsstatics/templates/rst content
 sphinx-build -b html resource-gcsstatics/docs content
 
+# commit content
+git clone resource-gcsstatics statics
+cp -r content statics
+
+cd statics
+
+#git config --global user.email "ci@greenwood.ng"
+git config --global user.email "cibot@greenwood.ng"
+git config --global user.name "Concourse"
+
+git add .
+
+git commit -m 'build v1.0.0'
+
+
 # minimize assets
 #grunt prod
